fabbd7c4ad8959911bcdc5c2efff07e6cc80d4de
hprandi <henriqueprandi@gmail.com>
2024-12-01 20:00:42 -0300

    Implemented draft movement

--- src/main/kotlin/data/world/Thing.kt
+++ src/main/kotlin/data/world/Thing.kt
@@ -3,8 +3,12 @@ package data.world
 sealed interface Thing {
     val image: String
 
-    object Grass : Thing {
+    data object Grass : Thing {
         override val image: String = "file:resources/grass.png"
+    }
+
+    data object Barbarian : Thing {
+        override val image: String = "file:resources/barbarian.gif"
     }
 }
 
--- src/main/kotlin/ui/Main.kt
+++ src/main/kotlin/ui/Main.kt
@@ -1,9 +1,13 @@
 package com.hprandi.lot.ui
 
 import com.hprandi.lot.world.World
+import data.world.Thing
 import javafx.application.Application
 import javafx.geometry.Rectangle2D
 import javafx.scene.Scene
+import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyEvent
+import javafx.scene.layout.Pane
 import javafx.stage.Screen
 import javafx.stage.Stage
 import javafx.stage.StageStyle
@@ -12,24 +16,57 @@ import ui.WorldDrawer
 class GameApplication : Application() {
 
     private val world = World()
+    private lateinit var stage: Stage
+    private lateinit var scene: Scene
+    private val worldDrawer = WorldDrawer()
+
+    private var barbarianX = 10
+    private var barbarianY = 10
 
     override fun start(primaryStage: Stage) {
-        val screenBounds: Rectangle2D = Screen.getPrimary().visualBounds
-        val squareSize = screenBounds.height * 0.05
+        stage = primaryStage
+        val squareSize = calculateSquareSize()
         val sceneSize = World.size * squareSize
-        val worldDrawer = WorldDrawer()
-        val root = worldDrawer.draw(world, squareSize)
-        val scene = Scene(root, sceneSize, sceneSize)
-        setupStage(primaryStage, scene)
+
+        world.getSquare(barbarianX, barbarianY).things.add(Thing.Barbarian)
+
+        scene = Scene(Pane(), sceneSize, sceneSize)
+        scene.setOnKeyPressed { moveBarbarian(it) }
+        stage.setup(scene)
+        updateUi()
     }
 
-    private fun setupStage(primaryStage: Stage, scene: Scene) {
-        primaryStage.scene = scene
-        primaryStage.title = "Legend of Taelin"
-        primaryStage.isFullScreen = false
-        primaryStage.isResizable = false
-        primaryStage.initStyle(StageStyle.UNDECORATED)
-        primaryStage.show()
+    private fun moveBarbarian(it: KeyEvent) {
+        world.getSquare(barbarianX, barbarianY).things.removeLast()
+
+        when (it.code) {
+            KeyCode.UP -> barbarianY--
+            KeyCode.DOWN -> barbarianY++
+            KeyCode.LEFT -> barbarianX--
+            KeyCode.RIGHT -> barbarianX++
+            else -> {}
+        }
+        world.getSquare(barbarianX, barbarianY).things.add(Thing.Barbarian)
+        updateUi()
+    }
+
+    private fun updateUi() {
+        scene.root = worldDrawer.draw(world, calculateSquareSize())
+    }
+
+    private fun calculateSquareSize(): Double {
+        val screenBounds: Rectangle2D = Screen.getPrimary().visualBounds
+        val squareSize = screenBounds.height * 0.05
+        return squareSize
+    }
+
+    private fun Stage.setup(scene: Scene) {
+        this.scene = scene
+        title = "Legend of Taelin"
+        isFullScreen = false
+        isResizable = false
+        initStyle(StageStyle.UNDECORATED)
+        show()
     }
 
 
